696c69e61bf71d25a0f9071023b1346487e60e79
FATHIHIND <hind10bella.Fathi@gmail.com>
2021-01-29 00:59:32 +0100

    web pack dev

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import $ from 'jquery'
 import 'bootstrap'
 import './styles/styles.scss'
 
+const version = '4'
 
-$('#title').html('Bonjour de Webpack 4');
+$('#title').html(`Bonjour de Webpack ${version}`);
